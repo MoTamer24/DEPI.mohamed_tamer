60c8f2aa0e89d54140a4689577ef9e4c35d439fb
Mohamed Tamer <motamerfarh@gmail.com>
2024-08-24 03:49:08 +0300

    add ado.net project as a task1

---
+++ Ado.Net/Task1/Ado.Net/User.cs
@@ -0,0 +1,27 @@
+﻿
+
+namespace Ado.Net
+{
+    internal class User
+    {
+        public int? Id { get; set; }
+        public string? Name { get; set; }
+        public string? Password { get; set; }
+        public string? Email { get; set; }
+        public string? Status { get; set; }
+        public string? Address1 { get; set; }
+        public string? Address2 { get; set; }
+        public string? City { get; set; }
+        public string? State { get; set; }
+        public int? CountryId { get; set; }
+        public string? PostalCode { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public override string ToString()
+        {
+            return $"{Id} : {Name} : {Password} : {Email} :" +
+                $" {Status} : {Address1} : {Address2} : {City} : {CountryId} : {PostalCode} : {CreatedAt}";
+        }
+    }
+
+}
+
